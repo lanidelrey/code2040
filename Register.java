4cb77262fafb5a716fc2a40f2bf4b0499c909c94
S <s>
2014-12-28 13:14:11 -0700

    {"type":"TI", "runs":"0"}

---
+++ Register.java
@@ -0,0 +1,77 @@
+/*
+ * File: Register.java
+ * -------------------------------
+ * This class sends registration info to Code2040's website.
+ */
+
+import java.util.*;
+
+public class Register {
+
+	/** 
+	 * Constructor
+	 * This method takes care of any initialization needed for 
+	 * the database.
+	 */
+	public Register() {
+		database = new HashMap<String, FacePamphletProfile>();
+	}
+	
+	
+	/** 
+	 * This method adds the given profile to the database.  If the 
+	 * name associated with the profile is the same as an existing 
+	 * name in the database, the existing profile is replaced by 
+	 * the new profile passed in.
+	 */
+	public void addProfile(FacePamphletProfile profile) {
+		database.put(profile.getName(), profile);
+	}
+
+	
+	/** 
+	 * This method returns the profile associated with the given name 
+	 * in the database.  If there is no profile in the database with 
+	 * the given name, the method returns null.
+	 */
+	public FacePamphletProfile getProfile(String name) {
+		if (database.containsKey(name)) {
+			return database.get(name);
+		} else {
+			return null;
+		}
+	}
+	
+	
+	/** 
+	 * This method removes the profile associated with the given name
+	 * from the database.  It also updates the list of friends of all
+	 * other profiles in the database to make sure that this name is
+	 * removed from the list of friends of any other profile.
+	 * 
+	 * If there is no profile in the database with the given name, then
+	 * the database is unchanged after calling this method.
+	 */
+	public void deleteProfile(String name) {
+		if (database.containsKey(name)) {
+			database.remove(name);
+		}
+	}
+
+	
+	/** 
+	 * This method returns true if there is a profile in the database 
+	 * that has the given name.  It returns false otherwise.
+	 */
+	public boolean containsProfile(String name) {
+		if (database.containsKey(name)) {
+			return true;
+		} else {
+			return false;
+		}
+	}
+	
+	/**private instance variables**/
+	private Map<String, FacePamphletProfile> database;
+
+}
