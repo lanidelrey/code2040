f00de660379511484026740b93eece50ce7c6cf8
S <s>
2014-12-29 09:46:57 -0700

    {"type":"TI", "runs":"1"}

--- Register.java
+++ Register.java
@@ -10,26 +10,27 @@ import java.lang.*;
 
 public class Register {
 	
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 		Register register = new Register();
 		register.sendGet();
 		register.sendPost();
 	}
 
-	private void sendGet() {
+	private void sendGet() throws IOException {
 		System.out.println("sending http GET request");
 		
 		String url = "http://challenge.code2040.org/api/register";
-		/*URL obj = new URL(url);
-		HttpURLConnection con = */
+		URL obj = new URL(url);
+		HttpURLConnection con = (HttpURLConnection) obj.openConnection();
 		
 	}
 	
-	private void sendPost() {
+	private void sendPost() throws IOException {
 		System.out.println("sending http POST request");
 	}
 	
 	/**private instance variables**/
 	private String token;
+	private static final String USER_AGENT = "Mozilla/5.0";
 
 }
