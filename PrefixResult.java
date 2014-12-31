1a50fc4c3b73020fd4dc1f1914723402e251025e
S <s>
2014-12-31 15:09:35 -0700

    {"type":"TI", "runs":"0"}

--- PrefixResult.java
+++ PrefixResult.java
@@ -8,6 +8,14 @@ import java.util.*;
 
 public class PrefixResult {
 	
+	public String returnPrefix() {
+		return result.returnPrefix();
+	}
+	
+	public String[] returnArray() {
+		return result.returnArray();
+	}
+	
 	/**public instance variables**/
 	public Prefix result;
 }
