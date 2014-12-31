38c84d5322c386841cc8df149cae11684249a31c
S <s>
2014-12-31 15:01:56 -0700

    {"type":"TI", "runs":"1"}

---
+++ PrefixResult.java
@@ -0,0 +1,17 @@
+/*
+ * File: PrefixResult.java
+ * -------------------------------
+ * This class deserializes the resultant prefix challenge JSON.
+ */
+
+import java.util.*;
+
+public class PrefixResult {
+
+	public PrefixResult() {
+		
+	}
+	
+	/**public instance variables**/
+	public Prefix result;
+}
