3c61711ad3f1136ed16545af6897eaca580c97d4
S <s>
2014-12-31 17:40:31 -0700

    {"type":"TI", "runs":"1"}

--- InnerJSONJavaObj.java
+++ InnerJSONJavaObj.java
@@ -1,27 +1,23 @@
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
 /*
  * File: InnerJSONJavaObj.java
  * -------------------------------
  * This class stores deserialized JSON data for the prefix challenge.
+ * referred to http://goo.gl/Y8I0R9 
  */
 
 public class InnerJSONJavaObj {
 
-	public InnerJSONJavaObj() {
-		prefix = "";
-		array = new String[6];
+	public static final <T> T getFromJSON(String json, Class<T> clazz) {
+		return gson.fromJson(json, clazz);
 	}
 	
-	public String getPrefix() {
-		return prefix;
+	public static final <T> String toJSON(T clazz) {
+		return gson.toJson(clazz);
 	}
 	
-	public String[] getArray() {
-		return array;
-	}
-	
-	/**public instance variables**/
-	public String prefix;
-	public String[] array;
-	
-	/****/
+	/**private instance variables**/
+	private static Gson gson = new GsonBuilder().create();
 }
