e2fc81cdac3ac43b6227720eb50107996d93a216
S <s>
2015-01-01 15:52:46 -0700

    {"type":"TI", "runs":"0"}

---
+++ PrefixJSON.java
@@ -0,0 +1,20 @@
+/*
+ * File: PrefixJSON.java
+ * -------------------------------
+ * This class defines deserialized JSON data for the prefix challenge.
+ */
+
+import java.util.*;
+
+public class PrefixJSON {
+	
+	private Prefix result;
+	
+	public Prefix getResult() {
+		return result;
+	}
+	
+	public void setPrefixJSON(Prefix prefix) {
+		this.result = prefix;
+	}
+}
