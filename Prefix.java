62110d75a733344b7ddd6793d767d2bda3563d0b
S <s>
2015-01-01 15:54:18 -0700

    {"type":"TI", "runs":"0"}

--- Prefix.java
+++ Prefix.java
@@ -8,12 +8,8 @@ import java.util.*;
 
 public class Prefix {
 
-	public Prefix(String[] array, String prefix) {
-		this.prefix = prefix;
-		this.array = array;
-//		prefix = getPrefix(input);
-//		array = getWords(input);
-	}
+	private String prefix;
+	private String[] array;
 	
 	public String getPrefix() {
 		return prefix;
@@ -23,45 +19,11 @@ public class Prefix {
 		return array;
 	}
 	
-//	public String getPrefix(String input) {
-//		String key = "\"prefix\":\"";
-//		int keyLength = key.length();
-//		int firstIndex = input.indexOf(key) + keyLength;
-//		int lastIndex = input.indexOf("\"}}");
-//		
-//		for (int i = firstIndex; i < lastIndex; i++) {
-//			prefix += input.charAt(i);
-//		}
-//		
-//		System.out.println("prefix: " + prefix);
-//		return prefix;
-//	}
-//	
-//	public String[] getWords(String input) {
-//		int index1 = input.indexOf("[");
-//		int index2 = input.indexOf("]");
-//		String rawWords = "";
-//		ArrayList<String> arrayList = new ArrayList<String>();
-//		
-//		for(int i = index1 + 1; i < index2; i++) {
-//			rawWords += input.charAt(i);
-//		}
-//		
-//		StringTokenizer tokenizer = new StringTokenizer(rawWords, " ,\"");
-//		while (tokenizer.hasMoreTokens()) {
-//			arrayList.add(tokenizer.nextToken());
-//		}
-//		
-//		array = new String[arrayList.size()];
-//		for (int j = 0; j < arrayList.size(); j++) {
-//			array[j] = arrayList.get(j);
-//		}
-//		
-//		System.out.println("array: " + array);
-//		return array;
-//	}
+	public void setPrefix(String prefix) {
+		this.prefix = prefix;
+	}
 	
-	/**public instance variables**/
-	public String prefix;
-	public String[] array;
+	public void setArray(String[] array) {
+		this.array = array;
+	}
 }
