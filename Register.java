0139868f9166c9b63de0a6b06dd6bbaa1d78d0ba
S <s>
2015-01-01 17:54:42 -0700

    {"type":"TI", "runs":"0"}

--- Register.java
+++ Register.java
@@ -132,7 +132,7 @@ public class Register {
 		String json = register.getJson(timeUrl, tokenMap());
 		TimeJSON result = gson.fromJson(json, TimeJSON.class);
 		System.out.println(result.toString());
-		
+		result.getAnswer();
 	}
 	
 	/**static methods**/
