452c6b6eec31bf7058e5670014bee24025478c07
S <s>
2015-01-01 18:17:14 -0700

    {"type":"TI", "runs":"0"}

--- Needle.java
+++ Needle.java
@@ -22,42 +22,9 @@ public class Needle {
 	}
 	
 	private int getNeedleIndex(String input) {
-		return pullArray(input).indexOf(pullNeedle(input));
+		return array.indexOf(needle);
 	}
 	
-	private ArrayList<String> pullArray(String input) {
-		int index1 = input.indexOf("[");
-		int index2 = input.indexOf("]");
-		String hayString = "";
-		ArrayList<String> haystack = new ArrayList<String>();
-		
-		for (int i = index1 + 1; i < index2; i++) {
-			hayString += input.charAt(i);
-		}
-		
-		StringTokenizer tokenizer = new StringTokenizer(hayString, " ,\"");
-		while (tokenizer.hasMoreTokens()) {
-			haystack.add(tokenizer.nextToken());
-		}
-		
-		System.out.println("haystack arrayList: " + haystack);
-		return haystack;
-	}
-	
-	private String pullNeedle(String input) {
-		String needle = "";
-		String key = "\"needle\":\"";
-		int keyLength = key.length();
-		int index1 = input.indexOf("\"needle\":\"");
-		int index2 = input.indexOf("\"}}");
-		
-		for (int i = index1 + keyLength; i < index2; i++) {
-			needle += input.charAt(i);
-		}
-		
-		System.out.println("needle: " + needle);
-		return needle;
-	}
 	
 	/**public instance variables**/
 	public int needle = -1;
