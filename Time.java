85a391117c873abdc9400b98c2993b555bed9597
S <s>
2015-01-01 17:29:38 -0700

    {"type":"TI", "runs":"0"}

---
+++ Time.java
@@ -0,0 +1,29 @@
+/*
+ * File: Time.java
+ * -------------------------------
+ * This class defines deserialized JSON data for the datestamp challenge.
+ */
+
+import java.util.*;
+
+public class Time {
+	
+	private Prefix result;
+	
+	public Prefix getResult() {
+		return result;
+	}
+	
+	public void setPrefixJSON(Prefix prefix) {
+		this.result = prefix;
+	}
+	
+	public String[] getFinalArray() {
+		return result.searchPrefixes();
+	}
+	
+	@Override
+	public String toString() {
+		return "PrefixJSON [result = " + result + "]";
+	}
+}
